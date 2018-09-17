8fe837385fc58624ed6c2fea7e58cc8771df24fa
v-chanikya <chanikya4vr1@gmail.com>
2018-09-17 10:27:38 +0530

    included post request presently using mock server by swagger
    Signed-off-by: v-chanikya <chanikya4vr1@gmail.com>

--- number_chronology/game_end.js
+++ number_chronology/game_end.js
@@ -1,4 +1,5 @@
 import {get_element_storage, set_element_storage, destroy_storage} from './utils';
+import {game_id, posturl} from './game_params';
 export var game_endSceneConfig = {
     key: 'game_end',
     active: false,
@@ -29,9 +30,30 @@ function game_endCreate(){
     this.add.text(510,200,"Game over",style_end).setOrigin(0.5,0.5);
 	this.add.text(510,300,"Your score:"+this.score,style_score).setOrigin(0.5,0.5);
     this.add.text(510,400,"Restart game",style_restart).setOrigin(0.5,0.5).setInteractive().on("clicked",restart,this);
+	var data_to_send = {
+		session_id: "abcd",
+		game_id: game_id,
+		question_id: "q1",
+		game_time: get_element_storage("total_time"),
+		score: this.score,
+		data:{
+			final_score: this.score
+		}
+	}
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", posturl, true);
+	xhr.setRequestHeader('accept', 'application/json');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(data_to_send));
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState === 4 && xhr.status === 200){
+    			destroy_storage();
+			console.log(xhr.responseText);
+		}
+	}
+
 };
 
 function restart(){
-    destroy_storage();
     document.location.reload();
 }
--- number_chronology/game_params.js
+++ number_chronology/game_params.js
@@ -2,3 +2,4 @@
 // Game vars;
 export var num_of_elements = 100;
 export var game_id = "g1l1"
+export var posturl = "http://virtserver.swaggerhub.com/game_dev/game_data_oav2/1.0.0/send_data"
